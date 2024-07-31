3c8348cba3cba6d0d513fcf8af9c6e26c2e6d9f5
RomanAnoshenko <89294907361@mail.ru>
2024-07-31 23:34:29 +0900

    Add files via upload

---
+++ module_1_3peredelannoe.py
@@ -0,0 +1,9 @@
+name = "Roman"
+print("Name:"+name)
+age = 33
+print("Age:" +str(age))
+age2 = 1
+age = (age+age2)
+print("New age:"+str(age))
+is_student = True
+print("Is student:" + str(is_student))
